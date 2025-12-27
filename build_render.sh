4c81dfd119f270f124255b395f19adec94f8c13f
Melbride <melbrideb@gmail.com>
2025-12-27 18:59:23 +0300

    Fix deployment: Add Django settings module environment variable
    - Added export DJANGO_SETTINGS_MODULE=crisis_communication.settings
    - Ensures Django can find settings module in build environment
    - Fixes 'Unknown command: collectstatic' error on Render

--- build_render.sh
+++ build_render.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+export DJANGO_SETTINGS_MODULE=crisis_communication.settings
 python3 manage.py collectstatic --noinput
 
 # Try migrations, but don't fail if database is unreachable
