acdbc0cba3ae15b39f7e2170fd815020185e1f85
Melbride <melbrideb@gmail.com>
2025-12-27 18:52:46 +0300

    Fix deployment: Use python3 instead of python for Render
    - Updated build_render.sh to use python3 command
    - Render uses Python 3.13.4 by default
    - Fixes 'Unknown command: collectstatic' error

--- build_render.sh
+++ build_render.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Try migrations, but don't fail if database is unreachable
-python manage.py migrate --noinput || echo "Migration failed, will retry at runtime"
+python3 manage.py migrate --noinput || echo "Migration failed, will retry at runtime"
