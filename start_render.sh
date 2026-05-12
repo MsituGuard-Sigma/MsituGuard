993db1c3656b69c7f80fceb420c587c86cedfab2
Melbride <melbrideb@gmail.com>
2026-05-12 20:11:44 +0300

    Improve Gunicorn logging on Render

--- start_render.sh
+++ start_render.sh
@@ -11,4 +11,9 @@ echo "=== Loading county data ==="
 python manage.py load_county_data
 
 echo "=== Starting web server ==="
-exec gunicorn crisis_communication.wsgi:application --bind 0.0.0.0:${PORT:-10000}
+exec gunicorn crisis_communication.wsgi:application \
+	--bind 0.0.0.0:${PORT:-10000} \
+	--access-logfile - \
+	--error-logfile - \
+	--log-level info \
+	--capture-output
